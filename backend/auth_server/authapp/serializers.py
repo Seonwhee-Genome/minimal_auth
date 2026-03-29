718e8af31d327eeb5e1cedb13eec04f9b1ce7db9
Seonwhee Jin <quarklep@naver.com>
2026-03-29 22:02:51 +0900

    Password validation in backend server

--- backend/auth_server/authapp/serializers.py
+++ backend/auth_server/authapp/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,6 +11,18 @@ class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
+    
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("username_already_exists")
+        return value
+    
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except Exception as e:
+            raise serializers.ValidationError(e.messages)
+        return value
     
     
     def create(self, validated_data):

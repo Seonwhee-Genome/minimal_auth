63430ceacde016552e94e225a474e7e2d5aaff28
Seonwhee Jin <quarklep@naver.com>
2026-03-30 14:23:11 +0900

    Setting for backend in production : gunicorn, Dockerfile

--- backend/auth_server/auth_server/settings.py
+++ backend/auth_server/auth_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8b-^24*4=q86tf6hfmqydc@@n$l^lj+(su18_*#agpu7z9p#&u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
